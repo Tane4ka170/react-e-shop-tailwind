dcc51c96eeaef5e356774d6b61d48db979af9d71
Tane4ka170 <tane4ka170@gmail.com>
2024-12-13 16:16:43 +0200

    Added TailwindCSS config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        whiteText: "#fff",
+        blackText: "#000",
+        lightText: "#f5f5f5",
+        greenText: "#99c218",
+        redText: "#f53823",
+        skyText: "#2db2f2",
+      },
+      flex: {
+        full: "0 0 100%",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
