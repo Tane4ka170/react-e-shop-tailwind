c14304385799469005cb70517a68bb50b7f015c4
Tane4ka170 <tane4ka170@gmail.com>
2024-12-13 16:14:38 +0200

    Added basic layout with Header & Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
+import Container from "./ui/Container.jsx";
+import Footer from "./ui/Footer.jsx";
+import Header from "./ui/Header.jsx";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <Header />
+      <Container>
+        <h1>Estimated</h1>
+      </Container>
+      <Footer />
+    </main>
+  );
 };
 
 export default App;
