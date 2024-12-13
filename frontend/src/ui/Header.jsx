00af57a8d7fb77ddc3ff825a93efa6f2edad7641
Tane4ka170 <tane4ka170@gmail.com>
2024-12-13 19:35:11 +0200

    Enhanced Header with Search & Menu

--- frontend/src/ui/Header.jsx
+++ frontend/src/ui/Header.jsx
@@ -1,20 +1,49 @@
+import { useState } from "react";
 import { logo } from "../assets";
+import { CiSearch, CiShoppingBasket, CiStar, CiUser } from "react-icons/ci";
+import { IoIosClose } from "react-icons/io";
 
 const Header = () => {
+  const [searchText, setSearchText] = useState("");
   return (
     <div className="w-full bg-whiteText">
       <div className="max-w-screen-xl mx-auto h-20 flex items-center justify-between px-4 lg:px-0">
         {/* Logo */}
         <img src={logo} alt="logo" className="w-14 h-14" />
         {/* Searchbar */}
-        <div className="hidden md:inline-flex max-w-3xl w-full relative bg-green-500">
+        <div className="hidden md:inline-flex max-w-3xl w-full relative">
           <input
             type="text"
-            placeholder="Search products"
-            className="w-full flex-1 rounded-full"
+            placeholder="Search products..."
+            className="w-full flex-1 rounded-full text-green-900 text-lg placeholder:text-base placeholder:tracking-wide shadow-sm ring-1 ring-inset ring-green-300 placeholder:text-green-400 placeholder:font-normal focus:ring-2 focus:ring-blackText sm:text-sm px-4 py-2"
+            onChange={(e) => setSearchText(e.target.value)}
+            value={searchText}
           />
+          {searchText ? (
+            <IoIosClose
+              onClick={() => setSearchText("")}
+              className="absolute top-2.5 right-4 text-xl hover:text-emerald-500 cursor-pointer duration-200"
+            />
+          ) : (
+            <CiSearch className="absolute top-2.5 right-4 text-xl" />
+          )}
         </div>
         {/* Menubar */}
+        <div className="flex items-center gap-x-6">
+          <CiUser className="hover:text-skyText duration-200 cursor-pointer" />
+          <div className="relative block">
+            <CiStar className="hover:text-skyText duration-200 cursor-pointer" />
+            <span className="inline-flex items-center justify-center bg-redText text-whiteText absolute -top-1 -right-2 text-[9px] rounded-full w-4 h-4">
+              0
+            </span>
+          </div>
+          <div className="relative block">
+            <CiShoppingBasket className="hover:text-skyText duration-200 cursor-pointer" />
+            <span className="inline-flex items-center justify-center bg-redText text-whiteText absolute -top-1 -right-2 text-[9px] rounded-full w-4 h-4">
+              0
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
