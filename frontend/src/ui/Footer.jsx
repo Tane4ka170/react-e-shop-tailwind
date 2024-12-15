85351574e76607374ef1baa470281c6848300005
Tane4ka170 <tane4ka170@gmail.com>
2024-12-15 16:08:58 +0200

    Added incentives & payment icons

--- frontend/src/ui/Footer.jsx
+++ frontend/src/ui/Footer.jsx
@@ -1,5 +1,41 @@
+import { payment } from "../assets";
+import Container from "./Container";
+import FooterTop from "./FooterTop";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  const incentives = [
+    {
+      name: "Free shipping",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce/icons/icon-shipping-simple.svg",
+      description:
+        "It's not actually free we just price it into the products. Someone's paying for it, and it's not us.",
+    },
+    {
+      name: "10-year warranty",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce/icons/icon-warranty-simple.svg",
+      description:
+        "If it breaks in the first 10 years we'll replace it. After that you're on your own though.",
+    },
+    {
+      name: "Exchanges",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce/icons/icon-exchange-simple.svg",
+      description:
+        "If you don't like it, trade it to one of your friends for something of theirs. Don't send it here though.",
+    },
+  ];
+
+  return (
+    <div className="mt-10">
+      <FooterTop />
+      <Container className="flex flex-col md:flex-row items-center gap-4 justify-between">
+        <p>@2024 E-Shop. All rights reserved</p>
+        <img src={payment} alt="payments" className="object-cover" />
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
