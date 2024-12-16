77dc1359e6afb03d45cf24df3cac4479c7487266
Tane4ka170 <tane4ka170@gmail.com>
2024-12-16 18:59:23 +0200

    Added route for fetching blog data

---
+++ backend/routes/blogs.mjs
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { blogsData } from "../constants/index.mjs";
+
+const router = Router();
+
+router.get("/blogs", (req, res) => {
+  res.send(blogsData);
+});
