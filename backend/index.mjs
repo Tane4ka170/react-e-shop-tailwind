05d61d25712d5b4357dd3389084d7566f619460e
Tane4ka170 <tane4ka170@gmail.com>
2024-12-17 21:06:48 +0200

    Enabled dynamic routing & CORS

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,12 +1,33 @@
 import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import { fileURLToPath } from "url";
+import path from "path";
+import { readdirSync } from "fs";
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
+app.use(cors());
+app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const routesPath = path.resolve(__dirname, "./routes");
+const routeFiles = readdirSync(routesPath);
+routeFiles.map(async (file) => {
+  const routeModule = await import(`./routes/${file}`);
+  if (routeModule.default) {
+    app.use("/", routeModule.default);
+  } else {
+    console.error(`Route file ${file} does not export a default route.`);
+  }
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is running on ${port}`);
 });
