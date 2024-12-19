daa95250b3b6f3a2c0aa995c10c419789c1da38a
Tane4ka170 <tane4ka170@gmail.com>
2024-12-19 21:06:17 +0200

    Added HomeBanner component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import BannerCategories from "./ui/BannerCategories.jsx";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import HomeBanner from "./ui/HomeBanner.jsx";
 
 const App = () => {
   return (
     <main>
       <BannerCategories />
+      <HomeBanner />
     </main>
   );
 };
