0aff179e27c60cf0817a1225cb8c8d0a941e70eb
Tane4ka170 <tane4ka170@gmail.com>
2024-12-20 17:07:11 +0200

    Added Highlights component to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import BannerCategories from "./ui/BannerCategories.jsx";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import HomeBanner from "./ui/HomeBanner.jsx";
+import Highlights from "./ui/Highlights.jsx";
 
 const App = () => {
   return (
     <main>
       <BannerCategories />
       <HomeBanner />
+      <Highlights />
     </main>
   );
 };
