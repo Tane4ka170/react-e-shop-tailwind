6212bee5cf2a765792c41bcbca00949a61da5f35
Tane4ka170 <tane4ka170@gmail.com>
2024-12-25 16:27:44 +0200

    Added brand logos to assets

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,5 +1,21 @@
 import logo from "./logo.png";
 import payment from "./payment.webp";
 import homeBanner from "./homeBanner.avif";
+import brandOne from "./brands/brandOne.png";
+import brandTwo from "./brands/brandTwo.png";
+import brandThree from "./brands/brandThree.png";
+import brandFour from "./brands/brandFour.png";
+import brandFive from "./brands/brandFive.png";
+import brandSix from "./brands/brandSix.png";
 
-export { logo, payment, homeBanner };
+export {
+  logo,
+  payment,
+  homeBanner,
+  brandOne,
+  brandTwo,
+  brandThree,
+  brandFour,
+  brandFive,
+  brandSix,
+};
