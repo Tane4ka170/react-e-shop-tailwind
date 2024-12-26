24bea9cc6d11cc3f775eca736d2168ba969855b5
Tane4ka170 <tane4ka170@gmail.com>
2024-12-26 16:09:39 +0200

    EnhancedDiscountedBanner

--- frontend/src/ui/DiscountedBanner.jsx
+++ frontend/src/ui/DiscountedBanner.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "./Container";
 import Title from "./Title";
 import { Link } from "react-router";
+import { discountImgOne, discountImgTwo } from "../assets";
 
 const DiscountedBanner = () => {
   const popularSearchItems = [
@@ -31,7 +32,28 @@ const DiscountedBanner = () => {
       </div>
 
       <div className="w-full py-5 md:py-0 my-12 bg-[#f9a09b] rounded-lg flex items-center justify-between overflow-hidden">
-        <img src="" alt="" />
+        <img
+          src={discountImgOne}
+          alt="discountImgOne"
+          className="hidden lg:inline-flex h-36"
+        />
+        <div>
+          <div className="flex items-center justify-center gap-x-3 text-xl md:text-4xl font-bold">
+            <h2>Sony Headphones</h2>
+            <Link
+              to={"/product"}
+              className="border border-lime-600 px-4 py-2 text-xl md:text-3xl text-lime-600 rounded-full"
+            >
+              Discount 20%
+            </Link>
+          </div>
+          <p>Whether you're heading out to play or to create</p>
+        </div>
+        <img
+          src={discountImgTwo}
+          alt="discountImgTwo"
+          className="hidden lg:inline-flex h-36"
+        />
       </div>
     </Container>
   );
