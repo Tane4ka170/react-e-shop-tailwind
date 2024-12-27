a5933e73207acfdb0d373278c6bf1796cd576ccd
Tane4ka170 <tane4ka170@gmail.com>
2024-12-27 19:37:50 +0200

    Enhanced DiscountedBanner with brands

--- frontend/src/ui/DiscountedBanner.jsx
+++ frontend/src/ui/DiscountedBanner.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import Container from "./Container";
 import Title from "./Title";
 import { Link } from "react-router";
-import { discountImgOne, discountImgTwo } from "../assets";
+import {
+  brandFive,
+  brandFour,
+  brandOne,
+  brandSix,
+  brandThree,
+  brandTwo,
+  discountImgOne,
+  discountImgTwo,
+} from "../assets";
 
 const DiscountedBanner = () => {
   const popularSearchItems = [
@@ -37,7 +46,7 @@ const DiscountedBanner = () => {
           alt="discountImgOne"
           className="hidden lg:inline-flex h-36"
         />
-        <div>
+        <div className="flex flex-col flex-1 gap-1 items-center">
           <div className="flex items-center justify-center gap-x-3 text-xl md:text-4xl font-bold">
             <h2>Sony Headphones</h2>
             <Link
@@ -47,13 +56,63 @@ const DiscountedBanner = () => {
               Discount 20%
             </Link>
           </div>
-          <p>Whether you're heading out to play or to create</p>
+          <p className="text-sm text-gray-600 font-medium">
+            Whether you're heading out to play or to create
+          </p>
         </div>
         <img
           src={discountImgTwo}
           alt="discountImgTwo"
           className="hidden lg:inline-flex h-36"
         />
+      </div>
+
+      <div className="mt-7">
+        <p className="font-bold text-2xl">Brands We Offer</p>
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 mt-7">
+          <div className="border border-r-0 border-red-300 flex items-center justify-center px-6 py-2 cursor-pointer">
+            <img
+              src={brandOne}
+              alt="Microsoft"
+              className="w-36 h-auto group-hover:opacity-45 duration-200"
+            />
+          </div>
+          <div className="border border-r-0 border-red-300 flex items-center justify-center px-6 py-2 cursor-pointer">
+            <img
+              src={brandTwo}
+              alt="Microsoft"
+              className="w-36 h-auto group-hover:opacity-45 duration-200"
+            />
+          </div>
+          <div className="border border-r-0 border-red-300 flex items-center justify-center px-6 py-2 cursor-pointer">
+            <img
+              src={brandThree}
+              alt="Microsoft"
+              className="w-36 h-auto group-hover:opacity-45 duration-200"
+            />
+          </div>
+          <div className="border border-r-0 border-red-300 flex items-center justify-center px-6 py-2 cursor-pointer">
+            <img
+              src={brandFour}
+              alt="Microsoft"
+              className="w-36 h-auto group-hover:opacity-45 duration-200"
+            />
+          </div>
+          <div className="border border-r-0 border-red-300 flex items-center justify-center px-6 py-2 cursor-pointer">
+            <img
+              src={brandFive}
+              alt="Microsoft"
+              className="w-36 h-auto group-hover:opacity-45 duration-200"
+            />
+          </div>
+          <div className="border border-r-0 border-red-300 flex items-center justify-center px-6 py-2 cursor-pointer">
+            <img
+              src={brandSix}
+              alt="Microsoft"
+              className="w-36 h-auto group-hover:opacity-45 duration-200"
+            />
+          </div>
+        </div>
       </div>
     </Container>
   );
