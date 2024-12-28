96dfbee3f6184e9f8975397e25bdd4ec84bee41c
Tane4ka170 <tane4ka170@gmail.com>
2024-12-28 17:23:03 +0200

    Implement Product Data Fetching with Loading State

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { config } from "../../config";
+import { getData } from "../lib";
 
 const Product = () => {
+  const [productsData, setProductsData] = useState(null);
+  const [allProducts, setAllProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
   const { id } = useParams();
-  const endpoint = id ? `${config?.baseUrl}` : "";
+  const endpoint = id
+    ? `${config?.baseUrl}/products/${id}`
+    : `${config?.baseUrl}/products/`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const data = await getData(endpoint);
+        if (id) {
+          setProductsData(data);
+          setAllProducts([]);
+        }
+      } catch (error) {
+        console.error("Failed to retrieve data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [id, endpoint]);
+
   return <div>Product</div>;
 };
 
