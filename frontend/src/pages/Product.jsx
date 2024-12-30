5384e5f2072a7cabd372aa27fc46034200b01aab
Tane4ka170 <tane4ka170@gmail.com>
2024-12-30 16:45:41 +0200

    Addeddynamicproductdetailslayout

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import _, { divide } from "lodash";
+import { CiStar } from "react-icons/ci";
+import { FaEye, FaRegWindowClose } from "react-icons/fa";
 import { config } from "../../config";
 import { getData } from "../lib";
 import Loading from "../ui/Loading";
 import Container from "../ui/Container";
+import PriceTag from "../ui/PriceTag";
+import FormattedPrice from "../ui/FormattedPrice";
+import AddToCartBtn from "../ui/AddToCartBtn";
+import ProductCard from "../ui/ProductCard";
 
 const Product = () => {
   const [productsData, setProductsData] = useState(null);
@@ -72,16 +78,122 @@ const Product = () => {
                   <img src={imgUrl} alt="mainImage" />
                 </div>
               </div>
-
               <div className="flex flex-col gap-4">
                 <h2 className="text-3xl font-bold">{productsData?.name}</h2>
+                <div className="flex items-center justify-between">
+                  <PriceTag
+                    regularPrice={productsData?.regularPrice}
+                    discountedPrice={productsData?.discountedPrice}
+                    className="text-xl"
+                  />
+                  <div className="flex items-center gap-1">
+                    <div className="text-base text-lightText flex items-center">
+                      <CiStar />
+                      <CiStar />
+                      <CiStar />
+                      <CiStar />
+                      <CiStar />
+                    </div>
+                    <p className="text-base font-semibold">{`(${productsData?.reviews} reviews)`}</p>
+                  </div>
+                </div>
+                <p className="flex items-center">
+                  <FaEye className="mr-1" />{" "}
+                  <span className="font-semibold mr-1">
+                    {productsData?.reviews}
+                  </span>{" "}
+                  people are looking at this right now
+                </p>
+                <p>
+                  You are saving{" "}
+                  <span className="text-base font-semibold text-green-500">
+                    <FormattedPrice
+                      amount={
+                        productsData?.regularPrice -
+                        productsData?.discountedPrice
+                      }
+                    />
+                  </span>{" "}
+                  upon purchase
+                </p>
                 <div>
-                  <PriceTag />
+                  {color && (
+                    <p>
+                      Color:{" "}
+                      <span
+                        className="font-semibold capitalize"
+                        style={{ color: color }}
+                      >
+                        {color}
+                      </span>
+                    </p>
+                  )}
+                  <div className="flex items-center gap-x-3">
+                    {productsData?.colors.map((item) => (
+                      <div
+                        key={item}
+                        className={`${
+                          item === color
+                            ? "border border-black p-1 rounded-full"
+                            : "border-transparent"
+                        }`}
+                      >
+                        <div
+                          className="w-10 h-10 rounded-full cursor-pointer"
+                          style={{ backgroundColor: item }}
+                          onClick={() => setColor(item)}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                  {color && (
+                    <button
+                      onClick={() => setColor("")}
+                      className="font-semibold mt-1 flex items-center gap-1 hover:text-red-600 duration-200"
+                    >
+                      <FaRegWindowClose /> Clear
+                    </button>
+                  )}
+                </div>
+                <p>
+                  Brand:{" "}
+                  <span className="font-medium">{productsData?.brand}</span>
+                </p>
+                <p>
+                  Category:{" "}
+                  <span className="font-medium">{productsData?.category}</span>
+                </p>
+                <AddToCartBtn
+                  product={productsData}
+                  title="Buy now"
+                  className="bg-black/80 py-3 text-base text-gray-200 hover:scale-100 hover:text-white duration-200"
+                />
+                <div className="bg-[#f7f7f7] p-5 rounded-md flex flex-col items-center justify-center gap-2">
+                  {/* <img
+                    src={productPayment}
+                    alt="payment"
+                    className="w-auto object-cover"
+                  /> */}
+                  <p className="font-semibold">
+                    Guaranteed safe & secure checkout
+                  </p>
                 </div>
               </div>
             </div>
           ) : (
-            <div>All Products</div>
+            <div className="flex items-start gap-10">
+              {/* <CategoryFilters id={id} /> */}
+              <div>
+                <p className="text-4xl font-semibold mb-5 text-center">
+                  Products Collection
+                </p>
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+                  {allProducts?.map((item) => (
+                    <ProductCard item={item} key={item?._id} />
+                  ))}
+                </div>
+              </div>
+            </div>
           )}
         </Container>
       )}
