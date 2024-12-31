fc75923347a5baebe17a66ee9b3a31e70224a404
Tane4ka170 <tane4ka170@gmail.com>
2024-12-31 17:01:01 +0200

    Added product prop to AddToCartBtn

--- frontend/src/ui/AddToCartBtn.jsx
+++ frontend/src/ui/AddToCartBtn.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { twMerge } from "tailwind-merge";
 
-const AddToCartBtn = ({ className, title }) => {
+const AddToCartBtn = ({ className, title, product }) => {
   const newClassName = twMerge(
     "bg-[#cc0aa6] uppercase text-xs py-3 text-center rounded-full font-semibold hover:bg-black hover:text-white hover:scale-105 duration-200 cursor-pointer",
     className
   );
-  return <button className={newClassName}>{title}</button>;
+  return (
+    <button className={newClassName}>{title ? title : "Add to cart"}</button>
+  );
 };
 
 export default AddToCartBtn;
