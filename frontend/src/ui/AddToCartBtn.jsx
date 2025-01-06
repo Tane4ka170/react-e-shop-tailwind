ec0f19c67e68e540e6ccc69e2fc46abeeb40f709
Tane4ka170 <tane4ka170@gmail.com>
2025-01-06 17:12:19 +0200

    Added cart functionality to AddToCartBtn

--- frontend/src/ui/AddToCartBtn.jsx
+++ frontend/src/ui/AddToCartBtn.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import { twMerge } from "tailwind-merge";
+import { store } from "../lib/store";
+import toast from "react-hot-toast";
 
 const AddToCartBtn = ({ className, title, product }) => {
   const newClassName = twMerge(
     "bg-[#cc0aa6] uppercase text-xs py-3 text-center rounded-full font-semibold hover:bg-black hover:text-white hover:scale-105 duration-200 cursor-pointer",
     className
   );
+
+  const { addToCart, cartProduct } = store();
+  // console.log("cartProduct", cartProduct);
+
+  const handleAddToCart = () => {
+    if (product) {
+      addToCart(product);
+      toast.success(`${product?.name.substring(0, 10)} added successfully`);
+    } else {
+      toast.error("The product is not defined");
+    }
+  };
   return (
-    <button className={newClassName}>{title ? title : "Add to cart"}</button>
+    <button className={newClassName} onClick={handleAddToCart}>
+      {title ? title : "Add to cart"}
+    </button>
   );
 };
 
