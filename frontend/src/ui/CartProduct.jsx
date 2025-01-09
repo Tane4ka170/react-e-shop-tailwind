a2b470885052160f60ace0804b975dd8509bd26e
Tane4ka170 <tane4ka170@gmail.com>
2025-01-09 18:00:43 +0200

    EnhancedCartProduct: AddedCategoryPriceButton

--- frontend/src/ui/CartProduct.jsx
+++ frontend/src/ui/CartProduct.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "react-router";
+import FormattedPrice from "./FormattedPrice";
+import AddToCartBtn from "./AddToCartBtn";
+import { IoIosClose } from "react-icons/io";
 
 const CartProduct = ({ product }) => {
   return (
@@ -8,16 +11,36 @@ const CartProduct = ({ product }) => {
         <img
           src={product?.images[0]}
           alt="productImage"
-          className="h-24 w-24 rounded-md object-cover sm:h-48 sm:w-48 border border-skyText/30 hover:border-skyText duration-300"
+          className="h-24 w-24 rounded-md object-cover object-center sm:h-48 sm:w-48 border border-skyText/30 hover:border-skyText duration-300"
         />
       </Link>
       <div className="ml-4 flex flex-col justify-between sm:ml-6">
         <div className="relative pr-9 sm:grid sm:grid-cols-4 sm:gap-x-6 sm:pr-0">
           <div className="flex flex-col gap-1 col-span-3">
-            <h3>{product?.name.substring(0, 80)}</h3>
-            <p>
-              Brand: <span>{product?.brand}</span>
+            <h3 className="text-base font-semibold w-full">
+              {product?.name.substring(0, 80)}
+            </h3>
+            <p className="text-xs">
+              Brand: <span className="">{product?.brand}</span>
             </p>
+            <p className="text-xs">
+              Category: <span className="">{product?.category}</span>
+            </p>
+            <div className="flex items-center gap-6 mt-2">
+              <p className="text-base font-semibold">
+                <FormattedPrice
+                  amount={product?.discountedPrice * product?.quantity}
+                />
+                <AddToCartBtn product={product} />
+              </p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <button>
+                <IoIosClose />
+              </button>
+            </div>
           </div>
         </div>
       </div>
