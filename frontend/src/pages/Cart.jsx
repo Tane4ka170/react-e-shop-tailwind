8421b6012492287fbbd0057cba7c97e04100a059
Tane4ka170 <tane4ka170@gmail.com>
2025-01-09 17:10:52 +0200

    UpdatedCartLayout: EnhancedGridStructure

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -13,15 +13,15 @@ const Cart = () => {
           <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Shopping Cart
           </h1>
-          <div>
-            <section>
-              <div>
+          <div className="mt-10 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16">
+            <section className="lg:col-span-7">
+              <div className="divide-y divide-gray-600 border-b border-t border-gray-200">
                 {cartProduct.map((item) => (
                   <CartProduct product={item} key={product?._id} />
                 ))}
               </div>
             </section>
-            <section></section>
+            <section className="mt-16 rounded-lg bg-gray-50 px-4 "></section>
           </div>
         </>
       ) : (
