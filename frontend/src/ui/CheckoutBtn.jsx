824e3a19052b3fcba6e2376018366a5cf8789aad
Tane4ka170 <tane4ka170@gmail.com>
2025-01-11 14:32:20 +0200

    EnhancedCheckoutButton: UserFeedbackIntegration

--- frontend/src/ui/CheckoutBtn.jsx
+++ frontend/src/ui/CheckoutBtn.jsx
@@ -3,7 +3,27 @@ import { store } from "../lib/store";
 
 const CheckoutBtn = ({ products }) => {
   const { currentUser } = store();
-  return <div>{currentUser ? <button></button> : ""}</div>;
+  return (
+    <div className="mt-6">
+      {currentUser ? (
+        <button
+          type="submit"
+          className="w-full rounded-md border border-transparent bg-gray-800 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-black focus:outline-none focus:ring-2 focus:ring-skyText focus:ring-offset-2 focus:ring-offset-gray-50 duration-200"
+        >
+          Checkout
+        </button>
+      ) : (
+        <button className="w-full text-base text-white text-center rounded-md border border-transparent bg-gray-500 px-4 py-3 cursor-not-allowed">
+          Checkout
+        </button>
+      )}
+      {!currentUser && (
+        <p className="mt-2 text-sm font-medium text-red-500 text-center">
+          You need to sign in to complete the checkout
+        </p>
+      )}
+    </div>
+  );
 };
 
 export default CheckoutBtn;
