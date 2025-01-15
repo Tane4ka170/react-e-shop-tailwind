2f3f51eb8c8a12feed24a5eb4f00b0b715ee72f7
Tane4ka170 <tane4ka170@gmail.com>
2025-01-15 19:09:29 +0200

    Adjusted container max-width

--- frontend/src/ui/Container.jsx
+++ frontend/src/ui/Container.jsx
@@ -3,7 +3,7 @@ import { twMerge } from "tailwind-merge";
 
 const Container = ({ children, className }) => {
   const newClassName = twMerge(
-    "max-w-screen mx-auto py-10 px-4 lg:px-0",
+    "max-w-screen-xl mx-auto py-10 px-4 lg:px-0",
     className
   );
   return <div className={newClassName}>{children}</div>;
