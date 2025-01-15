03d6698f78435a7f5f430ac6813ea74a840ce59b
Tane4ka170 <tane4ka170@gmail.com>
2025-01-15 19:08:39 +0200

    Created Profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { store } from "../lib/store";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../lib/firebase";
+import Container from "../ui/Container";
+import UserInfo from "../ui/UserInfo";
+import Registration from "../ui/Registration";
+import Loading from "../ui/Loading";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser, getUserInfo, isLoading } = store();
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      getUserInfo(user?.uid);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [getUserInfo]);
+
+  return (
+    <Container>
+      {currentUser ? <UserInfo /> : <Registration />}
+      {isLoading && <Loading />}
+    </Container>
+  );
 };
 
 export default Profile;
