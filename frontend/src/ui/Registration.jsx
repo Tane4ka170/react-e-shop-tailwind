6a63cd804135ba2dcf366432b1cc8ac24d3a6123
Tane4ka170 <tane4ka170@gmail.com>
2025-01-16 16:05:16 +0200

    Added Label component to Registration

--- frontend/src/ui/Registration.jsx
+++ frontend/src/ui/Registration.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Login from "./Login";
+import Label from "./Label";
 
 const Registration = () => {
   const [login, setLogin] = useState(true);
@@ -10,8 +11,21 @@ const Registration = () => {
       ) : (
         <div className="bg-gray-950 rounded-lg">
           <form className="max-w-5xl mx-auto pt-10 px-10 lg:px-0 text-white">
-            <div>
-              <h2>Registration Form</h2>
+            <div className="border-b border-b-white/10 pb-5">
+              <h2 className="text-lg font-semibold uppercase leading-7">
+                Registration Form
+              </h2>
+              <p className="mt-1 text-sm leading-6 text-gray-600">
+                You must provide the necessary information to register with us
+              </p>
+            </div>
+
+            <div className="border-b border-b-white/10 pb-5">
+              <div>
+                <div>
+                  <Label title="First name" htmlFor="firstName" />
+                </div>
+              </div>
             </div>
           </form>
         </div>
