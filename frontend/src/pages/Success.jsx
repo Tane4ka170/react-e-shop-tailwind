776fa907f7f8fef39c7b09116062bd202e5aa4d5
Tane4ka170 <tane4ka170@gmail.com>
2025-01-22 16:30:57 +0200

    Added success page

--- frontend/src/pages/Success.jsx
+++ frontend/src/pages/Success.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { store } from "../lib/store";
+import { useLocation, useNavigate } from "react-router";
 
 const Success = () => {
+  const { currentUser, cartProduct, resetCart } = store();
+  const location = useLocation();
+  const sessionId = new URLSearchParams(location.search).get("session_id");
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   return <div>Success</div>;
 };
 
