b49321006c660518be3c71fd904b2a80bdeea1a0
Tane4ka170 <tane4ka170@gmail.com>
2025-01-22 15:48:21 +0200

    Implemented Stripe checkout session creation

--- backend/routes/checkout.mjs
+++ backend/routes/checkout.mjs
@@ -1,10 +1,29 @@
 import { Router } from "express";
+import Stripe from "stripe";
 
 const router = Router();
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 
 router.post("/checkout", async (req, res) => {
+  const stripeKey = new Stripe(stripeSecretKey, {});
   try {
-  } catch (error) {}
+    const { items, email } = await req.body;
+    const extractingItems = await items.map((item) => {
+      quantity: item.quantity;
+      priceData: {
+        currency: "usd";
+        unit_amount: item.discountedPrice * 100;
+        productData: {
+          name: item.name;
+          description: item.description;
+          images: item.images;
+        }
+      }
+    });
+    const session = await stripe.checkout.sessions.create();
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
-export default
+export default router;
