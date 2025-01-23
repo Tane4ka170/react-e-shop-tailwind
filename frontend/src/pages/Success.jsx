2ea221d644f7c43652ad7a59598a51312d25b660
Tane4ka170 <tane4ka170@gmail.com>
2025-01-23 14:25:24 +0200

    Added order data saving and error handling

--- frontend/src/pages/Success.jsx
+++ frontend/src/pages/Success.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { store } from "../lib/store";
 import { useLocation, useNavigate } from "react-router";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../lib/firebase";
+import toast from "react-hot-toast";
 
 const Success = () => {
   const { currentUser, cartProduct, resetCart } = store();
@@ -8,6 +11,27 @@ const Success = () => {
   const sessionId = new URLSearchParams(location.search).get("session_id");
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!sessionId) {
+      navigate("/");
+    } else if (cartProduct.length > 0) {
+      const saveOrder = async () => {
+        try {
+          setLoading(true);
+          const orderRef = doc(db, "orders", currentUser?.email);
+          const docSnap = await getDoc(orderRef);
+          if (docSnap) {
+          }
+        } catch (error) {
+          toast.error("Error saving order data");
+        } finally {
+          setLoading(false);
+        }
+      };
+    }
+  }, [sessionId]);
+
   return <div>Success</div>;
 };
 
