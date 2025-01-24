67f4620bde9b5f8d30258b71c75ffb48894dbba0
Tane4ka170 <tane4ka170@gmail.com>
2025-01-24 20:06:24 +0200

    Added order fetching logic

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { store } from "../lib/store";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../lib/firebase";
 
 const Orders = () => {
+  const { currentUser } = store();
+  const [orders, setOrders] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      setLoading(true);
+      try {
+        const docRef = doc(db, "orders", currentUser?.email);
+        const docSnap = await getDoc();
+      } catch (error) {
+        console.log("Data fetching error", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+  }, [third]);
+
   return <div>Orders</div>;
 };
 
